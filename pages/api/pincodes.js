57475a3c80c5bc80aaf1860daf1419a8beee9844
Manav Kaushal <manavkaushal756@gmail.com>
2022-04-21 02:53:13 +0530

    pdp page complete

---
+++ pages/api/pincodes.js
@@ -0,0 +1,3 @@
+export default function handler(req, res) {
+  res.status(200).json([160020, 192581, 110000]);
+}
--- pages/product-view/[slug].js
+++ pages/product-view/[slug].js
@@ -1,11 +1,367 @@
+import { ProductCard } from "@components/ProductCard";
+import { Disclosure, RadioGroup, Tab } from "@headlessui/react";
+import {
+  HeartIcon,
+  MinusSmIcon,
+  PlusSmIcon,
+  StarIcon,
+} from "@heroicons/react/outline";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+const product = {
+  name: "Zip Tote Basket",
+  price: "$140",
+  rating: 4,
+  images: [
+    {
+      id: 1,
+      name: "Angled view",
+      src: "https://tailwindui.com/img/ecommerce-images/product-page-03-product-01.jpg",
+      alt: "Angled front view with bag zipped and handles upright.",
+    },
+  ],
+  colors: [
+    {
+      name: "Washed Black",
+      bgColor: "bg-gray-700",
+      selectedColor: "ring-gray-700",
+    },
+    { name: "White", bgColor: "bg-white", selectedColor: "ring-gray-400" },
+    {
+      name: "Washed Gray",
+      bgColor: "bg-gray-500",
+      selectedColor: "ring-gray-500",
+    },
+  ],
+  description: `
+    <p>The Zip Tote Basket is the perfect midpoint between shopping tote and comfy backpack. With convertible straps, you can hand carry, should sling, or backpack this convenient and spacious bag. The zip top and durable canvas construction keeps your goods protected for all-day use.</p>
+  `,
+  details: [
+    {
+      name: "Features",
+      items: [
+        "Multiple strap configurations",
+        "Spacious interior with top zip",
+        "Leather handle and tabs",
+        "Interior dividers",
+        "Stainless strap loops",
+        "Double stitched construction",
+        "Water-resistant",
+      ],
+    },
+  ],
+};
+
+const relatedProducts = [
+  {
+    id: 1,
+    name: "Zip Tote Basket",
+    color: "White and black",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/product-page-03-related-product-01.jpg",
+    imageAlt:
+      "Front of zip tote bag with white canvas, black canvas straps and handle, and black zipper pulls.",
+    price: "$140",
+  },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 const Slug = () => {
   const router = useRouter();
   const { query } = router;
+  const [open, setOpen] = useState(false);
+  const [pincode, setPincode] = useState(null);
+  const [isDeliverable, setIsDeliverable] = useState(null);
+  const [selectedColor, setSelectedColor] = useState(product.colors[0]);
 
-  return <div>Slug is {query.slug}</div>;
+  const handlePincodeInputChange = (e) => {
+    setPincode(e.target.value);
+  };
+
+  async function checkDeliveryByPincode() {
+    const res = await fetch("http://localhost:4000/api/pincodes");
+    const pincodes = await res.json();
+
+    if (pincodes.includes(parseInt(pincode))) {
+      setIsDeliverable(true);
+    } else {
+      setIsDeliverable(false);
+    }
+  }
+
+  return (
+    <>
+      <main className="max-w-7xl mx-auto sm:pt-16 sm:px-6 lg:px-8">
+        <div className="max-w-2xl mx-auto lg:max-w-none">
+          {/* Product */}
+          <div className="lg:grid lg:grid-cols-2 lg:gap-x-8 lg:items-start">
+            {/* Image gallery */}
+            <Tab.Group as="div" className="flex flex-col-reverse">
+              {/* Image selector */}
+              <div className="hidden mt-6 w-full max-w-2xl mx-auto sm:block lg:max-w-none">
+                <Tab.List className="grid grid-cols-4 gap-6">
+                  {product.images.map((image) => (
+                    <Tab
+                      key={image.id}
+                      className="relative h-24 bg-white rounded-md flex items-center justify-center text-sm font-medium uppercase text-gray-900 cursor-pointer hover:bg-gray-50"
+                    >
+                      {({ selected }) => (
+                        <>
+                          <span className="sr-only">{image.name}</span>
+                          <span className="absolute inset-0 rounded-md overflow-hidden">
+                            <img
+                              src={image.src}
+                              alt=""
+                              className="w-full h-full object-center object-cover"
+                            />
+                          </span>
+                          <span
+                            className={classNames(
+                              selected ? "ring-primary" : "ring-transparent",
+                              "absolute inset-0 rounded-md ring-2 ring-offset-2 pointer-events-none"
+                            )}
+                            aria-hidden="true"
+                          />
+                        </>
+                      )}
+                    </Tab>
+                  ))}
+                </Tab.List>
+              </div>
+
+              <Tab.Panels className="w-full aspect-w-1 aspect-h-1">
+                {product.images.map((image) => (
+                  <Tab.Panel key={image.id}>
+                    <img
+                      src={image.src}
+                      alt={image.alt}
+                      className="w-full h-full object-center object-cover sm:rounded-lg"
+                    />
+                  </Tab.Panel>
+                ))}
+              </Tab.Panels>
+            </Tab.Group>
+
+            {/* Product info */}
+            <div className="mt-10 px-4 sm:px-0 sm:mt-16 lg:mt-0">
+              <h1 className="text-3xl font-extrabold tracking-tight text-gray-900">
+                {product.name}
+              </h1>
+
+              <div className="mt-3">
+                <h2 className="sr-only">Product information</h2>
+                <p className="text-3xl text-gray-900">{product.price}</p>
+              </div>
+
+              {/* Reviews */}
+              <div className="mt-3">
+                <h3 className="sr-only">Reviews</h3>
+                <div className="flex items-center">
+                  <div className="flex items-center">
+                    {[0, 1, 2, 3, 4].map((rating) => (
+                      <StarIcon
+                        key={rating}
+                        className={classNames(
+                          product.rating > rating
+                            ? "text-primary"
+                            : "text-gray-300",
+                          "h-5 w-5 flex-shrink-0"
+                        )}
+                        aria-hidden="true"
+                      />
+                    ))}
+                  </div>
+                  <p className="sr-only">{product.rating} out of 5 stars</p>
+                </div>
+              </div>
+
+              <div className="mt-6">
+                <h3 className="sr-only">Description</h3>
+                <div
+                  className="text-base text-gray-700 space-y-6"
+                  dangerouslySetInnerHTML={{ __html: product.description }}
+                />
+              </div>
+
+              <form className="mt-6">
+                {/* Colors */}
+                <div>
+                  <RadioGroup
+                    value={selectedColor}
+                    onChange={setSelectedColor}
+                    className="mt-2"
+                  >
+                    <RadioGroup.Label className="sr-only">
+                      Choose a color
+                    </RadioGroup.Label>
+                    <div className="flex items-center space-x-3">
+                      {product.colors.map((color) => (
+                        <RadioGroup.Option
+                          key={color.name}
+                          value={color}
+                          className={({ active, checked }) =>
+                            classNames(
+                              color.selectedColor,
+                              active && checked ? "ring ring-offset-1" : "",
+                              !active && checked ? "ring-2" : "",
+                              "-m-0.5 relative p-0.5 rounded-full flex items-center justify-center cursor-pointer focus:outline-none"
+                            )
+                          }
+                        >
+                          <RadioGroup.Label as="p" className="sr-only">
+                            {color.name}
+                          </RadioGroup.Label>
+                          <span
+                            aria-hidden="true"
+                            className={classNames(
+                              color.bgColor,
+                              "h-8 w-8 border border-black border-opacity-10 rounded-full"
+                            )}
+                          />
+                        </RadioGroup.Option>
+                      ))}
+                    </div>
+                  </RadioGroup>
+                </div>
+
+                {/* Pincode */}
+                <div className="pincode mt-6 text-gray-700">
+                  <label htmlFor="postal-code" className="text-base">
+                    Check Delivery
+                  </label>
+                  <div className="mt-1 flex items-center space-x-4">
+                    <input
+                      type="number"
+                      name="postal-code"
+                      id="postal-code"
+                      value={pincode?.slice(0, 6)}
+                      onChange={(e) => handlePincodeInputChange(e)}
+                      autoComplete="postal-code"
+                      className="focus:outline-none focus:ring-0 focus:border-gray-800 relative block w-56 rounded-md bg-transparent focus:z-10 sm:text-sm border-gray-300"
+                      placeholder="ZIP / Postal code"
+                      required
+                    />
+                    <button
+                      type="button"
+                      className="btn-ghost transition-200 capitalize"
+                      onClick={checkDeliveryByPincode}
+                      disabled={pincode?.length === 0 ? true : false}
+                    >
+                      Check
+                    </button>
+                  </div>
+                  {isDeliverable && isDeliverable != null && (
+                    <div className="text-emerald-600 mt-2">
+                      Product is deliverable to your location
+                    </div>
+                  )}
+                  {!isDeliverable && isDeliverable != null && (
+                    <div className="text-red-600 mt-2">
+                      Sorry! we do not offer delivery services to your location.
+                    </div>
+                  )}
+                </div>
+
+                <div className="mt-10 flex sm:flex-col1">
+                  <button
+                    type="submit"
+                    className="max-w-xs flex-1 btn-black sm:w-full"
+                  >
+                    Add to bag
+                  </button>
+                  <button
+                    type="button"
+                    className="ml-4 py-3 px-3 rounded-md flex items-center justify-center text-gray-400 hover:bg-gray-100 hover:text-gray-500"
+                  >
+                    <HeartIcon
+                      className="h-6 w-6 flex-shrink-0"
+                      aria-hidden="true"
+                    />
+                    <span className="sr-only">Add to favorites</span>
+                  </button>
+                </div>
+              </form>
+
+              <section aria-labelledby="details-heading" className="mt-12">
+                <h2 id="details-heading" className="sr-only">
+                  Additional details
+                </h2>
+
+                <div className="border-t divide-y divide-gray-200">
+                  {product.details.map((detail) => (
+                    <Disclosure as="div" key={detail.name} defaultOpen>
+                      {({ open }) => (
+                        <>
+                          <h3>
+                            <Disclosure.Button className="group relative w-full py-4 flex justify-between items-center text-left">
+                              <span
+                                className={classNames(
+                                  open ? "text-primary" : "text-gray-900",
+                                  "text-lg font-semibold"
+                                )}
+                              >
+                                {detail.name}
+                              </span>
+                              <span className="ml-6 flex items-center">
+                                {open ? (
+                                  <MinusSmIcon
+                                    className="block h-6 w-6 text-primary group-hover:text-primary"
+                                    aria-hidden="true"
+                                  />
+                                ) : (
+                                  <PlusSmIcon
+                                    className="block h-6 w-6 text-gray-400 group-hover:text-gray-500"
+                                    aria-hidden="true"
+                                  />
+                                )}
+                              </span>
+                            </Disclosure.Button>
+                          </h3>
+                          <Disclosure.Panel
+                            as="div"
+                            className="pb-6 prose prose-sm"
+                          >
+                            <ul role="list">
+                              {detail.items.map((item) => (
+                                <li key={item}>{item}</li>
+                              ))}
+                            </ul>
+                          </Disclosure.Panel>
+                        </>
+                      )}
+                    </Disclosure>
+                  ))}
+                </div>
+              </section>
+            </div>
+          </div>
+
+          <section
+            aria-labelledby="related-heading"
+            className="mt-10 border-t border-gray-200 py-16 px-4 sm:px-0"
+          >
+            <h2
+              id="related-heading"
+              className="text-xl font-bold text-gray-900"
+            >
+              Customers also bought
+            </h2>
+
+            <div className="mt-8 grid grid-cols-2 gap-y-8 gap-x-4 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 xl:gap-x-6">
+              {relatedProducts.map((product) => (
+                <ProductCard data={product} />
+              ))}
+            </div>
+          </section>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default Slug;
