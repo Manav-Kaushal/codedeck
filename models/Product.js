37499ffdad80dbcb45ae84ead848f565dd4c0fa3
Manav Kaushal <manavkaushal756@gmail.com>
2022-04-24 08:26:38 +0530

    order, product and user schema created

---
+++ models/Product.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const ProductSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    description: { type: String, required: true },
+    imageSrc: { type: String, required: true },
+    category: { type: String, required: true },
+    size: { type: String },
+    color: { type: String },
+    price: { type: Number, required: true },
+    availableQty: { type: Number, required: true },
+  },
+  { timeStamps: true }
+);
+
+export default mongoose.model("Product", ProductSchema);
