2bdde13b34ea44252340bea44deb53d4c48ac98a
Manav Kaushal <manavkaushal756@gmail.com>
2022-05-01 09:57:49 +0530

    passwords encrypted in signup/signin flow

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,11 +1,17 @@
 import connectDB from "../../middleware/mongoose";
 import User from "../../models/User";
+var CryptoJS = require("crypto-js");
 
 const handler = async (req, res) => {
   if (req.method == "POST") {
     let user = await User.findOne({ email: req.body.email });
+    const bytes = CryptoJS.AES.decrypt(user.password, "secret key 123");
+    const decryptedPassword = bytes.toString(CryptoJS.enc.Utf8);
     if (user) {
-      if (req.body.email == user.email && req.body.password == user.password) {
+      if (
+        req.body.email == user.email &&
+        req.body.password == decryptedPassword
+      ) {
         res
           .status(200)
           .json({ success: true, email: user.email, name: user.name });
--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,9 +1,15 @@
 import connectDB from "../../middleware/mongoose";
 import User from "../../models/User";
+var CryptoJS = require("crypto-js");
 
 const handler = async (req, res) => {
   if (req.method == "POST") {
-    let newUser = new User(req.body);
+    const { name, email } = req.body;
+    let newUser = new User({
+      name,
+      email,
+      password: CryptoJS.AES.encrypt(req.body.password, "secret key 123").toString(),
+    });
     await newUser.save();
     res.status(200).json({ success: true });
   } else {
